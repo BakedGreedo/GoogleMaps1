bf1548cbe6c6fddea0e5105cf75e9e3a1241f875
PrestonMott <prestonmott@gmail.com>
2017-05-30 11:08:08 -0700

    Added GPS and Network location

--- MyMaps/app/src/main/java/com/example/mottp7116/mymaps/MapsActivity.java
+++ MyMaps/app/src/main/java/com/example/mottp7116/mymaps/MapsActivity.java
@@ -1,11 +1,16 @@
 package com.example.mottp7116.mymaps;
 
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,10 +18,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     boolean type = true;
+    private LocationManager locationManager;
+    private boolean isGPSenabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+    private static final long MIN_TIME_BW_UPDATES = 1000*15;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 5.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,5 +78,39 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //get GPS status
+            isGPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if(isGPSenabled) Log.d("MyMaps", "getLocation: GPS is enabled");
+
+            //get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if(isNetworkEnabled) Log.d("MyMaps", "getLocation: Network is enabled");
+
+            if(!isGPSenabled && !isNetworkEnabled) {
+                Log.d("MyMap", "getLocation: No provider is enabled!");
+            } else {
+                canGetLocation = true;
+                if (isGPSenabled) {
+                    Log.d("MyMaps", "getLocation: GPS enabled - Requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerGps);
+                    Log.d("MyMaps", "getLocation: Network GPS update request is successful");
+                    Toast.makeText(this, "Using GPS", Toast.LENGTH_SHORT);
+                }
+                if (isNetworkEnabled) {
+                    Log.d("MyMaps", "getLocation: Network enabled - Requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    Log.d("MyMaps", "getLocation: Network GPS update request is successful");
+                    Toast.makeText(this, "Using Neetwork", Toast.LENGTH_SHORT);
+                }
+            }
+        } catch (Exception e) {
+            Log.d("MyMaps", "Caught an exception in getLocation");
+            e.printStackTrace();
+        }
+    }
 
 }
